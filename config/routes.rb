d6d70a59eafeff9c7f63d00820ab8187c67c6acb
rchaser53 <tayoshizawa29@gmail.com>
2017-03-26 05:06:26 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'application#hello'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get  'static_pages/contact'
+
+  #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
