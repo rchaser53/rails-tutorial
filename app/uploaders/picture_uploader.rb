901f11566378f541750c0d0c6b690d8b431f9067
rchaser53 <tayoshizawa29@gmail.com>
2017-04-02 08:51:28 +0000

    completed

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,16 +1,21 @@
+
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
-  # アップロードファイルの保存先ディレクトリは上書き可能
-  # 下記はデフォルトの保存先  
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # アップロード可能な拡張子のリスト
+  # Add a white list of extensions which are allowed to be uploaded.
   def extension_white_list
     %w(jpg jpeg gif png)
   end
